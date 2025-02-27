26d8a211e57c2ef519736f7ab400068217454b6a
Nikolai Kocev <nikolai_kocev@hotmail.com>
2025-02-27 19:30:46 +0100

    testing some tailwind

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import './styles.css';
 function App() {
   return (
     <>
-      <h1 className='text-3xl font-bold underline'> Hello TEST</h1>
+      <h1 className='text-3xl font-bold underline'>Hello PPC</h1>
     </>
   );
 }

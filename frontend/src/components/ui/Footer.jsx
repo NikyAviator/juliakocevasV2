bae555410a93983919c17f277e176d82693d9598
Nikolai Kocev <nikolai_kocev@hotmail.com>
2025-03-09 20:04:42 +0100

    Follow Julia on social media is to the left of icons

--- frontend/src/components/ui/Footer.jsx
+++ frontend/src/components/ui/Footer.jsx
@@ -2,7 +2,11 @@ const Footer = () => {
   return (
     <footer className='bg-gray-900 absolute w-full'>
       <div className='mx-auto max-w-7xl px-6 py-12 md:flex md:items-center md:justify-between lg:px-8'>
-        <div className='flex justify-center gap-x-6 md:order-2'>
+        <div className='flex items-center gap-x-4 md:order-2'>
+          {/* Text to the left of icons */}
+          <p className='text-sm text-gray-400'>Follow Julia on social media:</p>
+
+          {/* Social Media Icons */}
           <a
             href='https://www.instagram.com/juliakocevas'
             target='_blank'
@@ -10,9 +14,9 @@ const Footer = () => {
             className='text-gray-400 hover:text-gray-300'
           >
             <span className='sr-only'>Instagram</span>
-
-            <img src='/svg/instagram.svg' alt='TikTok' className='w-6 h-6' />
+            <img src='/svg/instagram.svg' alt='Instagram' className='w-6 h-6' />
           </a>
+
           <a
             href='https://www.tiktok.com/@juliakocevas?_t=ZN-8uVb8GrSvsk&_r=1'
             target='_blank'
@@ -23,7 +27,9 @@ const Footer = () => {
             <img src='/svg/tiktok.svg' alt='TikTok' className='w-6 h-6' />
           </a>
         </div>
-        <p className='mt-8 text-center text-sm/6 text-gray-400 md:order-1 md:mt-0'>
+
+        {/* Copyright Text */}
+        <p className='mt-8 text-center text-sm text-gray-400 md:order-1 md:mt-0'>
           &copy; 2025 Made by: NikyAviator. All rights reserved.
         </p>
       </div>

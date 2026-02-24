0b1c73cd70f90578b265b81837ec782edf3d8997
Nikolai Kocev <nikolai_kocev@hotmail.com>
2026-02-24 20:49:57 +0100

    fixed footer mobile

--- frontend/src/components/ui/Footer.jsx
+++ frontend/src/components/ui/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer className='bg-gray-900 absolute w-full'>
-      <div className='mx-auto max-w-7xl px-6 py-12 md:flex md:items-center md:justify-between lg:px-8'>
+      <div className='mx-auto max-w-7xl px-6 py-12 sm:justify-center-safe md:flex md:items-center md:justify-between lg:px-8'>
         <div className='flex items-center gap-x-4 md:order-2'>
           {/* Text to the left of icons */}
           <p className='text-sm text-gray-400'>Follow Julia on social media:</p>
@@ -29,8 +29,8 @@ const Footer = () => {
         </div>
 
         {/* Copyright Text */}
-        <p className='mt-8 text-center text-sm text-gray-400 md:order-1 md:mt-0'>
-          &copy; 2025 Made by: NikyAviator. All rights reserved.
+        <p className='mt-8 text-left text-sm text-gray-400 md:order-1 md:mt-0'>
+          &copy; 2026 Made by: NikyAviator. All rights reserved.
         </p>
       </div>
     </footer>
